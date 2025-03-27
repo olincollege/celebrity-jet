09788b5ab857c061ffa461919030dac0b4ab5a4c
julakshah <julakshah@gmail.com>
2025-03-27 14:49:47 -0400

    added data formatting

--- manipulate_data.py
+++ manipulate_data.py
@@ -4,6 +4,31 @@ from bs4 import BeautifulSoup
 import keys
 
 
+def get_apininjas_data(celeb_name):
+    """
+    Given a string name of a celebrity returns the api call to apininjas celeb
+    api.
+
+    Args:
+        celeb_name: A string name of a celebrity that data will be collected for
+
+    Returns:
+        Dictionary of information of a given celebrity.
+    """
+    api_key = keys.get_ninja_key()
+    api_url = "https://api.api-ninjas.com/v1/celebrity?name={}".format(
+        celeb_name
+    )
+    response = requests.get(
+        api_url,
+        headers={"X-Api-Key": api_key},
+    )
+    if response.status_code == requests.codes.ok:
+        return response
+    else:
+        print("Error:", response.status_code, response.text)
+
+
 def get_flighttracking(
     url="https://celebrityprivatejettracker.com/leaderboard/",
     file_path="Data/data_flighttracker.html",
@@ -57,7 +82,6 @@ def get_celeb_chunks(file_path="Data/data_flighttracker.html"):
         soup = BeautifulSoup(file, "html.parser")
         elements = soup.find_all("tr", class_="trlight")
 
-    # print(elements)
     return elements[:-1]
 
 
@@ -74,34 +98,35 @@ def get_celeb_data(celeb_html):
     Returns:
         A list of lists containing celebrity information (see above).
     """
-    # STILL NEED TO BREAK THIS INTO SMALLER FUNCTIONS
-    name_list = {}
+    data_dict = {}
     for chunk in celeb_html:
-        name = chunk.find("a", class_="maincolor")  # .text.strip()
-        if name is None:
-            name = chunk.find("td")
-            name_list[name.text.strip()] = get_data_points(chunk)
-        else:
-            name_list[name.text.strip()] = get_apininjas_data(chunk)
-        # name = chunk.find("td").text.strip()
-    clean_names = []
-    for i, n in enumerate(name_list):
-        clean_names.append(n.text.strip())
-
-    celeb_data = dict.fromkeys(clean_names)
-
-    return remove_duplicate_names(clean_names)
+        # get the name from chunk
+        clean_name = get_individual_name(chunk)
+        # get associated data from chunk
+        clean_data = get_individual_data(chunk)
+        # combine name and data into a dictionary
+        data_dict[clean_name] = clean_data
+    return data_dict
 
 
-def get_data_points(chunk):
-    values = chunk.find_all("td")[1::]  # all but first one
-    print(values)
-    return values
+def get_individual_name(celeb_chunk):
+    """
+    Given a chunk of celebrity data, finds the name of the celebrity
 
+    Args:
+        celeb_chunk: a bs4 object of celebrity data
 
-def remove_duplicate_names(name_list):
-    unique_names = list(dict.fromkeys(name_list))
-    return unique_names
+    Returns:
+        A string with the celebrity name and some designation (often)
+    """
+    # find the name catagory
+    name = celeb_chunk.find("a", class_="maincolor")  # .text.strip()
+    if name is None:
+        name = celeb_chunk.find("td")
+
+    # clean the name
+    clean_name = name.get_text(strip=True)
+    return clean_name
 
 
 def get_individual_data(celeb_chunk):
@@ -110,37 +135,100 @@ def get_individual_data(celeb_chunk):
     them as a list.
 
     Args:
-        celeb_chunk: a soup chunk of celebrity data
+        celeb_chunk: a bs4 object of celebrity data
 
     Returns:
         A list of strings containing celebrity data
     """
-    print(celeb_chunk)
+    # find the data attached to the name
+    data_points = celeb_chunk.find_all("td")[1::]
+    clean_data = []
+    for n in data_points:
+        if n.find("a") is not None:
+            clean_data.append(str(n.find("a").get_text(strip=True)))
+        else:
+            clean_data.append(str(n.get_text(strip=True)))
+    return clean_data
 
-    # co2 = person_soup.find_all("td")[-1].get_text(strip=True).split("m")[0]
-    # print(co2)
+
+def clean_all_data(data_dict):
+    for key in data_dict:
+        clean_data(data_dict[key])
 
 
-def get_apininjas_data(celeb_name):
+def clean_data(data_list):
     """
-    Given a string name of a celebrity returns the api call to apininjas celeb
-    api.
+    Converts a list of celebrity data into usable number strings.
 
-    Args:
-        celeb_name: A string name of a celebrity that data will be collected for
-
-    Returns:
-        Dictionary of information of a given celebrity.
+    Args: A list of celebrity data
     """
-    api_key = keys.get_ninja_key()
-    api_url = "https://api.api-ninjas.com/v1/celebrity?name={}".format(
-        celeb_name
-    )
-    response = requests.get(
-        api_url,
-        headers={"X-Api-Key": api_key},
-    )
-    if response.status_code == requests.codes.ok:
-        return response
-    else:
-        print("Error:", response.status_code, response.text)
+    # miles
+    clean_number(data_list, 2)
+    # gallons
+    clean_number(data_list, 4)
+    # co2
+    clean_number(data_list, 6)
+    clean_time(data_list)
+
+
+def clean_number(data_list, index):
+    """
+    Extracts the miles a celebrity's private jet has flown as a pure number.
+
+    Args: A list of celebrity data
+    """
+    value = ""
+    for char in data_list[index]:
+        if char.isdigit():
+            value = value + char
+
+    data_list[index] = value
+
+
+def clean_time(data_list):
+    """
+    Extracts the hours of flight time the celebrity has flown in their private
+    jet as a pure number
+
+    Args: A list of celebrity data
+    """
+    index = 5
+    dirty_time = data_list[index]
+    days = ""
+    hours = ""
+    minutes = ""
+
+    start_index = 0
+    number_end_index = 0
+    in_words = False
+
+    for i, char in enumerate(data_list[index]):
+        if in_words is False and (char.isalpha() or char == " "):
+            in_words = True
+            number_end_index = i
+        if (in_words is True and char.isdigit()) or (
+            i == (len(dirty_time) - 1)
+        ):
+            slice = dirty_time[start_index:i]
+            if slice.find("day") != -1:
+                days = dirty_time[start_index:number_end_index]
+            if slice.find("hour") != -1:
+                hours = dirty_time[start_index:number_end_index]
+            if slice.find("minute") != -1:
+                minutes = dirty_time[start_index:number_end_index]
+            start_index = i
+            in_words = False
+
+    if days == "":
+        days = 0
+    if hours == "":
+        hours = 0
+    if minutes == "":
+        minutes = 0
+
+    days = int(days)
+    hours = int(hours)
+    minutes = int(minutes)
+
+    in_hours = (days * 24) + hours + (minutes / 60)
+    data_list[index] = in_hours
